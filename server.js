58599941b8b01ae1113320ba902356d444850cc6
LeonardPuettmann <leopuettmann@gmail.com>
2023-10-18 21:48:23 +0200

    Continued with stock dashboard

--- server.js
+++ server.js
@@ -20,59 +20,33 @@ const { BlobServiceClient } = require('@azure/storage-blob');
 const AZURE_STORAGE_CONNECTION_STRING = 'your-azure-storage-connection-string';
 const containerName = 'your-container-name';
 
-app.get('/fetch-file/:date', async (req, res) => {
-  const blobName = req.params.date + '.txt'; // assuming the blobs are named as 'date.txt'
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-  try {
-    const downloadBlockBlobResponse = await blockBlobClient.download(0);
-    const fileContent = (await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)).toString();
-    res.send(fileContent);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-async function streamToBuffer(readableStream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    readableStream.on('data', (data) => {
-      chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-    });
-    readableStream.on('end', () => {
-      resolve(Buffer.concat(chunks));
-    });
-    readableStream.on('error', reject);
-  });
-}
+req.params.ticker 
 
 const { SecretClient } = require('@azure/keyvault-secrets');
 const { DefaultAzureCredential } = require('@azure/identity');
 
 // Add your Key Vault url
-const vaultUrl = 'https://your-key-vault-name.vault.azure.net/';
+const vaultUrl = 'https://mlgroup.vault.azure.net/';
 
 let credential = new DefaultAzureCredential();
 let client = new SecretClient(vaultUrl, credential);
 
 app.get('/fetch-file/:date', async (req, res) => {
-  const blobName = req.params.date + '.txt'; // assuming the blobs are named as 'date.txt'
+    const blobName = 'process-stock-news'+ '-' + req.params.date + '.json';
   
-  // Retrieve the connection string from Azure Key Vault
-  const secret = await client.getSecret('your-secret-name');
-  const AZURE_STORAGE_CONNECTION_STRING = secret.value;
+    // Retrieve the connection string from Azure Key Vault
+    const secret = await client.getSecret('blob-storage-key');
+    const AZURE_STORAGE_CONNECTION_STRING = secret.value;
 
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-  try {
-    const downloadBlockBlobResponse = await blockBlobClient.download(0);
-    const fileContent = (await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)).toString();
-    res.send(fileContent);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+    try {
+        const downloadBlockBlobResponse = await blockBlobClient.download(0);
+        const fileContent = (await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)).toString();
+        res.send(fileContent);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
